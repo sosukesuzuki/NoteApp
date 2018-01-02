3745709f9fb30489bd20916c05c18a6bddfb8c84
Sosuke Suzuki <aosukeke@gmail.com>
2018-01-02 10:44:04 +0900

    refactor webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,49 @@
 const path = require('path')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const MinifyPlugin = require('babel-minify-webpack-plugin')
 const webpack = require('webpack')
 
-module.exports = function webpackStuff (env) {
-  return {
-    entry: [
-      './src/index.js'
-    ],
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, './')
-    },
-    module: {
-      rules: [{
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            'es2015'
-          ],
-          plugins: []
-        },
-        include: [
-          path.resolve(__dirname, './')
-        ]
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './')
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+        presets: [
+          'es2015'
+        ],
+        plugins: []
+      },
+      include: [
+        path.resolve(__dirname, './')
+      ]
+    }, {
+      test: /\.styl$/,
+      include: __dirname + '/src',
+      exclude: /node_modules/,
+      use: [{
+        loader: 'style-loader',
+        options: { sourceMap: true }
       }, {
-        test: /\.styl$/,
-        include: __dirname + '/src',
-        exclude: /node_modules/,
-        use: [{
-          loader: 'style-loader',
-          options: { sourceMap: true }
-        }, {
-          loader: 'css-loader',
-          options: {
-            localIdentName: '[name]-[local]',
-            modules: true,
-            sourceMap: true
-          }
-        }, {
-          loader: 'stylus-loader',
-          options: { sourceMap: true }
-        }]
+        loader: 'css-loader',
+        options: {
+          localIdentName: '[name]-[local]',
+          modules: true,
+          sourceMap: true
+        }
+      }, {
+        loader: 'stylus-loader',
+        options: { sourceMap: true }
       }]
-    },
-    plugins: [
-      new MinifyPlugin()
-    ]
-  }
+    }]
+  },
+  plugins: [
+    new MinifyPlugin()
+  ]
 }
