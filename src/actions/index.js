87b6df59d1030eaa614d3c87b26640a8f950fc1b
Sosuke Suzuki <aosukeke@gmail.com>
2018-01-06 09:50:57 +0900

    implement createnote

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,22 @@ export default {
   updateFolderModalInput: (text) => (state) => ({folderModalContent: text}),
   createFolder: (name, color) => (state) => {
     const newState = Object.assign({}, state)
-    newState.folders.push({name, color, id: newState.folders[newState.folders.length - 1].id + 1})
+    newState.folders.push({
+      name,
+      color,
+      id: newState.folders[newState.folders.length - 1].id + 1
+    })
     return {folders: newState.folders}
   },
-  setFolderId: (folderId) => (state) => ({folderId})
+  setFolderId: (folderId) => (state) => ({folderId}),
+  createNote: (folderId) => (state) => {
+    const newState = Object.assign({}, state)
+    newState.notes.push({
+      title: 'Untitled',
+      contnet: '',
+      folderId,
+      id: newState.notes[newState.notes.length - 1].id + 1
+    })
+    return {notes: newState.notes}
+  }
 }
--- src/components/NoteList/index.js
+++ src/components/NoteList/index.js
@@ -2,10 +2,14 @@ import { h } from 'hyperapp'
 import s from './index.styl'
 import NoteList from './components/NoteList'
 
+const handleOnClick = (folderId, createNote) => {
+  createNote(folderId)
+}
 export default ({ state, actions }) =>
   <div class={s.notelist_root}>
     <div class={s.header}>
-      <button class={s.createbutton}>
+      <button onclick={() => handleOnClick(state.folderId, actions.createNote)}
+        class={s.createbutton}>
         create
       </button>
     </div>
