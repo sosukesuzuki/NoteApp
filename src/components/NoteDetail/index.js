e98da4d76df4a5c1064586f352535d84a45be348
Sosuke Suzuki <aosukeke@gmail.com>
2018-01-08 14:04:28 +0900

    implement toggle markdown mode

--- src/components/NoteDetail/index.js
+++ src/components/NoteDetail/index.js
@@ -3,8 +3,14 @@ import s from './index.styl'
 import MarkdownEditor from './components/MarkdownEditor'
 import MarkdownPreview from './components/MarkdownPreview'
 
+const handleOnContextMenu = (e, toggleMode) => {
+  e.preventDefault()
+  toggleMode()
+}
+
 export default ({state, actions}) =>
-  <div class={s.notedetail_root}>
+  <div class={s.notedetail_root}
+    oncontextmenu={(e) => handleOnContextMenu(e, actions.toggleMode)}>
     {
       state.mode === 'EDITOR'
         ? <MarkdownEditor />
